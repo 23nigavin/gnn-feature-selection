b8a8d7662beeb03c445ee8e366785a2fc66bc687
23nigavin <jialei.ni@gmail.com>
2026-05-03 18:45:11 -0500

    adapt pca to same model as AE

--- src/pca.py
+++ src/pca.py
@@ -2,9 +2,8 @@ import torch
 from sklearn.decomposition import PCA
 
 def apply_pca(feature_matrix, train_mask, n_components):
-    x_train = feature_matrix[train_mask].cpu().numpy()
     x_all = feature_matrix.cpu().numpy()
     pca = PCA(n_components=n_components, random_state=42)
-    pca.fit(x_train)
+    pca.fit(x_all)
     x_pca = pca.transform(x_all)
     return torch.tensor(x_pca, dtype=feature_matrix.dtype, device=feature_matrix.device)
